5ffdb31db713603d8989c3956a9fcdf8070699a2
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-08-31 14:54:37 +0300

    add moringa-prep-projects to github

---
+++ JavaScript/functions/main.js
@@ -0,0 +1,7 @@
+var saySomething = function(whatToSay) {
+alert(whatToSay);
+};
+var add = function(number1, number2) {
+return number1 + number2;
+};
+saySomething("The sum is " + add(3,5) + ".");
---
+++ JavaScript/simpler_functions/main.js
@@ -0,0 +1,14 @@
+//You use () if there are more than one ARGUMENT
+
+var add = (number1, number2) => number1 + number2;
+ var saySomething = (whatToSay) => alert(whatToSay);
+ saySomething("The sum is "+ add(3,4) + ".")
+
+
+//You can  OMIT () when you have ARGUMENT
+
+ /*var add = (number1, number2) => number1 + number2;
+ var saySomething = whatToSay => alert(whatToSay);
+ saySomething("The sum is "+ add(3,4) + ".")*/
+
+
