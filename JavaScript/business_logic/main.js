ee021e44397a0b6b1bf66bbbb223a670ae96c274
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-08-31 16:38:04 +0300

    add control_flows

--- JavaScript/business_logic/main.js
+++ JavaScript/business_logic/main.js
@@ -1,4 +1,19 @@
-var add = (number1,number2) => number1 + number2;
+/* var add = (number1,number2) => number1 + number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
+var subtract = (number1,number2) => number1 - number2;
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var multiply = (number1,number2) => number1 * number2;
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:")); */
+
+var divide = (number1,number2) => number1 / number2;
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+
+var result = divide(number1, number2);
+alert(result);
---
+++ JavaScript/control_flows/main.js
@@ -0,0 +1,21 @@
+  //tell if a number is even or odd
+var number = 8;
+
+if((number%2) === 0){
+    console.log("It's even");
+}else{
+    console.log("It's odd")
+}
+
+//choose btwn tea and coffee
+var tea = false;
+var coffee = true;
+var juice = false;
+
+if(tea){
+    console.log('We serve tea now');
+}else if(juice){
+    console.log('We serve juice now');
+}else{
+    console.log('We serve coffee now')
+}
